2b48219a0b68e1f001acfe1c577a751248968e22
Guidone <guido.bellomo@gmail.com>
2019-03-18 18:09:35 +0100

    cleanup methods

--- components/plugin.js
+++ components/plugin.js
@@ -6,17 +6,26 @@ export default class Plugin {
     this._views = {};
   }
 
-  registerView(region, view, props) {
+  register(region, view, props) {
+
+    if (region == null || region === '') {
+      throw new Error('Region is a required parameter');
+    }
+
+    if (arguments.length === 2) {
+      props = view;
+    } else if (arguments.length === 1) {
+      view = null;
+      props = {};
+    }
+
     if (this._views[region] == null) {
       this._views[region] = [];
     }
     // todo do some checks
     this._views[region].push({ view, props });
-    return this;
-  }
 
-  register(region, props) {
-    this.registerView(region, null, props);
+
     return this;
   }
 
